04e29cfed599362a599765013a7fda3caf97736a
ElPatoDelCuak <davidjuncosaalegre@gmail.com>
2026-01-23 17:01:22 +0100

    Intento de estilo

--- niko_niko/urls.py
+++ niko_niko/urls.py
@@ -6,4 +6,7 @@ app_name = 'niko_niko'
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
+    path('recipe/', views.recipe, name='recipe'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- niko_niko/views.py
+++ niko_niko/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'niko_niko/home.html')
+
+def recipe(request):
+    return render(request, 'niko_niko/recipe.html')
+
+def about(request):
+    return render(request, 'niko_niko/about.html')
+
+def contact(request):
+    return render(request, 'niko_niko/contact.html')
